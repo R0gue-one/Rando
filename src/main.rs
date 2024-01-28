7c29b2e0f83d7bf0410b40749b63fa1636d106ed
Harshit <harshitjaiswal2606@gmail.com>
2024-01-28 12:18:52 +0530

    P of 0 reduced

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,46 @@ use chrono::{Local, DateTime, Utc, Timelike};
 //     println!("{}", std::any::type_name::<T>())
 // }
 
+fn count_digits(mut num: u64) -> usize {
+    if num == 0 {
+        return 1; // Special case for 0
+    }
+
+    let mut count = 0;
+    while num > 0 {
+        num /= 10;
+        count += 1;
+    }
+    count
+}
+
 fn main() {
      
     for a in 1..10{
         let now = Local::now();
         let nano_sec = u64::from(now.nanosecond());
         let sec = now.second();
-        // print_type_of(&nano_sec);
         println!(" sec: {}, nanosecond: {}", sec, nano_sec);
         
         let random_no = rng(nano_sec, sec);
-        println!("{}", random_no);
+        println!();
+        println!("Final: {}, digits: {}", random_no, count_digits(random_no));
+        println!();
+
     }
 
-    let random = rng(34102765,20);
-    println!("{}",random);
 }
 
 fn rng(seed: u64, rounds: u32) -> u64{
     let mut r_no: u64 = seed;
     
     for x in 1..rounds {
-        if r_no == 0{
-            println!("0 mil gya in round: {}",x);
-        }
-        r_no = (r_no*r_no)%1_000_000;
+        // if r_no == 0{
+        //     println!("0 mil gya in round: {}",x);
+        // }
+        // println!("{}, {}",r_no*r_no/1000,r_no);
+        r_no = (r_no*r_no/1000)%1_000_000_000;
+        println!("{}",r_no);
     }
     // println!("Random no: {}", r_no);
     r_no
